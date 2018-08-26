c99274c40a53c1f5bdc7eb99890ab74d5af66bc3
SagarOnline <chopade.sagar@gmail.com>
2018-08-26 06:32:31 +0530

    Fixed tests.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,27 @@
 import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { EquityList } from './app.component';
+describe('EquityList', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        EquityList
       ],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EquityList);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
   it(`should have as title 'chartanalysis'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EquityList);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('chartanalysis');
   }));
   it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(EquityList);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to chartanalysis!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Chart Analysis');
   }));
 });
